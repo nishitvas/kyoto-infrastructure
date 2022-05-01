81ab020a6a4f99bf92c4be4b5d8247bd521046b2
Nishit Vas <nishit.vas@gmail.com>
2022-05-01 23:33:26 +0530

    Add check for environment variables

--- src/index.rb
+++ src/index.rb
@@ -2,6 +2,10 @@ require_relative "./core/users"
 require_relative "./core/docker"
 require_relative "./applications"
 
+unless ENV["KYOTO_ENV_LOADED"] == "LOADED"
+  raise "Kyoto environment variables are not set"
+end
+
 # Create login user
 Users.create_user(ENV["KYOTO_USER_NAME"], password: ENV["KYOTO_USER_PASS"], with_home_dir: true, groups: ["wheel"])
 
